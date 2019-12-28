829a015be3f5fda2300a58e3c777276ed21a125f
Aaron Burmeister <f34r91@gmail.com>
2019-12-28 06:54:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,19 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_data = {}
+pigeon_data.each {|attribute, descriptor|
+  descriptor.each {|sub_attribute, namelist| 
+    namelist.each {|name| 
+      if !new_data[name]
+        new_data[name] = {"#{attribute}": ["#{sub_attribute}"]}
+      elsif !new_data[name]["#{attribute}".to_sym]
+        new_data[name]["#{attribute}".to_sym] = ["#{sub_attribute}"]
+      else
+        new_data[name]["#{attribute}".to_sym] << "#{sub_attribute}"
+      end
+    }
+  }
+}
 end
 
 pigeon_data = {
